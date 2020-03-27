96ff900ea762585b68e9fe73aed7a27e0528f66d
MarioDena <enrique.dn@outlook.com>
2020-03-27 15:43:23 -0600

    [FIX, FEATURE] Linter comments, added win condition checking

--- script.js
+++ script.js
@@ -20,12 +20,42 @@ const gamefactory = () => {
   const innerCells = document.querySelectorAll('.inner-cell');
   let player;
 
+  const checkWinningConditions = (playerChecked) => {
+    const gameWinningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2],
+    ];
+    const playedConvos = gameBoard.reduce((acumulator, element, index) => (
+      (element === playerChecked) ? acumulator.concat(index) : acumulator), []);
+    let gameWon = null;
+    gameWinningConditions.forEach(elem => {
+      if (elem.every(win => playedConvos.indexOf(win) > -1)) {
+        gameWon = playerChecked;
+      }
+    });
+
+    return {
+      gameWon,
+    };
+  };
+
   const play = (cell) => {
-    gameBoard[cell] = player.symbol;
-    innerCells[cell].innerHTML = player.symbol;
+    const gameOver = checkWinningConditions(player.symbol).gameWon;
+    // eslint-disable-next-line no-unused-expressions
+    gameOver ? getId('restart').style.display = 'block' : (gameBoard[cell] = player.symbol, innerCells[cell].innerHTML = player.symbol);
   };
 
   const startBoard = () => {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < innerCells.length; i++) {
+      gameBoard[i] = i;
+    }
     innerCells.forEach(cell => { cell.innerHTML = ''; cell.addEventListener('click', play.bind(this, cell.id), false); });
   };
 
@@ -43,4 +73,5 @@ const gamefactory = () => {
   };
 };
 
+// eslint-disable-next-line no-unused-vars
 const game = gamefactory();
