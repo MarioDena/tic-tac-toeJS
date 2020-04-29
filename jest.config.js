19dcfd99be2f849c118854004816ab2d4e2e313c
MarioDena <enrique.dn@outlook.com>
2020-04-29 11:48:43 -0500

    [FIx] Fixed Linter spacing

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    // Flag to indicate if Code Coverage to be collected and reported
-    collectCoverage: true,
+  // Flag to indicate if Code Coverage to be collected and reported
+  collectCoverage: true,
 
-    // An array of glob patterns indicating a set of files for which coverage
-    // information should be collected
-    collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+  // An array of glob patterns indicating a set of files for which coverage
+  // information should be collected
+  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
 
-    // The directory where Jest should output its coverage files
-    coverageDirectory: 'coverage',
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
 
-    // Allows for a label to be printed along side a test while it is running.
-    displayName: {
-        name: 'AWESOME TESTING',
-        color: 'yellow',
-    },
+  // Allows for a label to be printed along side a test while it is running.
+  displayName: {
+    name: 'AWESOME TESTING',
+    color: 'yellow',
+  },
 };
