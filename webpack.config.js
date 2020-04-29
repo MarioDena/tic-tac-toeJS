c12d683c30cd943115dc02aea23dcbc0ab2a10c8
MarioDena <enrique.dn@outlook.com>
2020-04-29 11:53:29 -0500

    [FIx] Fixed linter errors on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/script.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/script.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
