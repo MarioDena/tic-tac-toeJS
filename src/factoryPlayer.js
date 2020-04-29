adaff0eff3b152660d7b52cd3f1a9896c619e95d
MarioDena <enrique.dn@outlook.com>
2020-04-29 11:51:01 -0500

    [FIx] Fixed src files linter errors

--- src/factoryPlayer.js
+++ src/factoryPlayer.js
@@ -1,6 +1,6 @@
 export default class PlayerFactory {
-    constructor(name) {
-        this.name = name;
-        this.symbol = 'O';
-    }
+  constructor(name) {
+    this.name = name;
+    this.symbol = 'O';
+  }
 }
